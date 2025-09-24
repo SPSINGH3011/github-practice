b9bc810f7ed27213394d199f25f09abd40f9a8f4
SPSINGH3011 <spsingh302212@gmail.com>
2025-09-24 07:50:14 +0530

    add 102rg

--- RG/main.tf
+++ RG/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "git-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "git102-rg"
+  location = "West Europe"
+}
