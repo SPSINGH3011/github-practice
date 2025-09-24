9f04d3c0b083359f4b491d7cb7470ab31c842242
Satya Prakash <spsingh302212@gmail.com>
2025-09-24 08:12:35 +0530

    Update main.tf

--- RG/main.tf
+++ RG/main.tf
@@ -1,20 +1,21 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg1" {
   name     = "git-rg"
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg2" {
   name     = "git102-rg"
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg3" {
   name     = "git101-rg"
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg4" {
   name     = "git100-rg"
   location = "West Europe"
 }
+
 
