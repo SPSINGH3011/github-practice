ee3db5c904a6a46da9409bae96cd6a088858a802
SPSINGH3011 <spsingh302212@gmail.com>
2025-09-24 08:08:14 +0530

    101

--- RG/main.tf
+++ RG/main.tf
@@ -3,9 +3,14 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
-
 resource "azurerm_resource_group" "rg" {
   name     = "git102-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "git101-rg"
+  location = "West Europe"
+}
+
 
